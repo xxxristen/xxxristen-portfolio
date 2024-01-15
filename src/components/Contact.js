a609a810016125f8fe62c38fa5590933586ffeac
Xristen <lim.christen@gmail.com>
2024-01-15 20:47:25 +0800

    shortened duration

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,7 +18,7 @@ export const Contact = () => {
         hidden: { opacity: 0, scale: 0 },
         show: {
             opacity: 1, scale: 1, transition: {
-                duration: 0.8,
+                duration: 0.5,
             }
         }
     }
