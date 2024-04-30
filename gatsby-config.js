a325d5e18aa5dc005e691760c4cdf7bf93271a7f
Xristen <lim.christen@gmail.com>
2024-04-30 23:26:02 +0800

    config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,7 @@ module.exports = {
             anonymize_ip: true,
             cookie_expires: 0,
           },
-          // This object is used for configuration specific to this plugin
+          // This object is used for configuration specific to gtag plugin
           pluginConfig: {
             // Puts tracking script in the head instead of the body
             head: false,
