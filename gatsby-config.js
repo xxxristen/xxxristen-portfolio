17a05085d2a3c080f66d64221fc88bbef025fae1
Xristen <lim.christen@gmail.com>
2024-04-30 22:46:33 +0800

    Updated plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,67 +6,79 @@ module.exports = {
     title: `xxxristen-portfolio`,
     siteUrl: `https://xxxristen-portfolio.vercel.app/`
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `xxxristen-portfolio`,
-        short_name: `x`,
-        start_url: `/`,
-        background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
-        display: `standalone`,
-        icon: `./src/images/favicon.png`,
+  plugins:
+    ["gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp",
+      {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+          "name": "images",
+          "path": "./src/images/"
+        },
+        __key: "images"
       },
-    },
-    {
-      resolve: "gatsby-omni-font-loader",
-      options: {
-        mode: "async",
+      {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: `xxxristen-portfolio`,
+          short_name: `x`,
+          start_url: `/`,
+          background_color: `#f7f0eb`,
+          theme_color: `#a2466c`,
+          display: `standalone`,
+          icon: `./src/images/favicon.png`,
+        },
+      },
+      {
+        resolve: "gatsby-omni-font-loader",
+        options: {
+          mode: "async",
 
-        /* Enable font loading listener to handle FOUT */
-        enableListener: true,
+          /* Enable font loading listener to handle FOUT */
+          enableListener: true,
 
-        /* Preconnect URL-s. This example is for Google Fonts */
-        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
-        web: [{
-          name: `Montserrat`,
-          file: `https://fonts.googleapis.com/css2?family=Montserrat`,
-        },
-        {
-          name: `Outfit`,
-          file: `https://fonts.googleapis.com/css2?family=Outfit`,
-        },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-gtag`,
-      options: {
-        trackingIds: ["G-BSNKGT4CGW", "GT-5TJS5VVT", "GTM-WDCJTZ52"],
-        gtagConfig: {
-          anonymize_ip: true,
-          cookie_expires: 0,
-        },
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          // exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // Delays processing pageview events on route update (in milliseconds)
-          delayOnRouteUpdate: 0,
+          /* Preconnect URL-s. This example is for Google Fonts */
+          preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+          web: [{
+            name: `Montserrat`,
+            file: `https://fonts.googleapis.com/css2?family=Montserrat`,
+          },
+          {
+            name: `Outfit`,
+            file: `https://fonts.googleapis.com/css2?family=Outfit`,
+          },
+          ],
         },
       },
-    },
-  ]
+      {
+        resolve: "gatsby-plugin-google-tagmanager",
+        options: {
+          id: process.env.GATSBY_GTM_CONTAINER,
+
+          // Include GTM in development.
+          // Defaults to false meaning GTM will only be loaded in production.
+          includeInDevelopment: false,
+        },
+      },
+      {
+        resolve: `gatsby-plugin-google-gtag`,
+        options: {
+          trackingIds: [process.env.GATSBY_GA4TAG1, process.env.GATSBY_GA4TAG2],
+          gtagConfig: {
+            anonymize_ip: true,
+            cookie_expires: 0,
+          },
+          // This object is used for configuration specific to this plugin
+          pluginConfig: {
+            // Puts tracking script in the head instead of the body
+            head: false,
+            // Setting this parameter is also optional
+            respectDNT: true,
+            // Avoids sending pageview hits from custom paths
+            // exclude: ["/preview/**", "/do-not-track/me/too/"],
+            // Delays processing pageview events on route update (in milliseconds)
+            delayOnRouteUpdate: 0,
+          },
+        },
+      },
+    ]
 };
